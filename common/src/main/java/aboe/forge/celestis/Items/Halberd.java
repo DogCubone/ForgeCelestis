420fd53486168c074a913de5385de27b8fa47d61
Kawã <kauamarinhodasilva1@gmail.com>
2024-12-06 22:16:11 -0300

    Its a good start

---
+++ common/src/main/java/aboe/forge/celestis/Items/Halberd.java
@@ -0,0 +1,43 @@
+package aboe.forge.celestis.Items;
+
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.SwordItem;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.crafting.Ingredient;
+
+public class Halberd extends SwordItem {
+
+    public Halberd(Tier tier, int i, float f, Properties properties) {
+        super(new Tier() {
+            @Override
+            public int getUses() {
+                return 0;
+            }
+
+            @Override
+            public float getSpeed() {
+                return 0;
+            }
+
+            @Override
+            public float getAttackDamageBonus() {
+                return 0;
+            }
+
+            @Override
+            public int getLevel() {
+                return 0;
+            }
+
+            @Override
+            public int getEnchantmentValue() {
+                return 0;
+            }
+
+            @Override
+            public Ingredient getRepairIngredient() {
+                return null;
+            }
+        }, 3, -3f, new Item.Properties());
+    }
+}
